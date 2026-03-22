77d8aeeecbbb8ec4057a4d8ef652c07f3a83bbfb
Aitor Conesa <aitorconesalucena@gmail.com>
2026-03-22 18:35:57 +0100

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import "./assets/index.css";
 import App from './App.jsx'
 
+// Load react-scan only in development
+if (import.meta.env.DEV) {
+  import('https://cdn.jsdelivr.net/npm/react-scan/dist/auto.global.js')
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
